a9d387bc08fbd5895d078eead01a3aa3ad3e59ab
txs1992 <xusheng.tao@ele.me>
2019-03-05 23:03:28 +0800

    feat：压缩打包文件。

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import vue from 'rollup-plugin-vue'
 import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/menu-sidebar/index.js',
@@ -8,5 +9,9 @@ export default {
     format: 'umd',
     name: 'MenuSidebar'
   },
-  plugins: [commonjs(), vue()]
+  plugins: [
+    commonjs(),
+    vue(),
+    terser()
+  ]
 }
