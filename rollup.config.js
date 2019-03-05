72a1244fba3b6652dd70a9bfc30cc0b8ec50686e
txs1992 <xusheng.tao@ele.me>
2019-03-05 22:30:21 +0800

    feat：使用 rollup umd 方式完成打包。

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import vue from 'rollup-plugin-vue'
+import commonjs from 'rollup-plugin-commonjs'
+
 export default {
   input: 'src/menu-sidebar/index.js',
   output: {
-    file: 'bundle.js',
-    format: 'cjs'
-  }
+    file: 'lib/index.js',
+    format: 'umd',
+    name: 'MenuSidebar'
+  },
+  plugins: [commonjs(), vue()]
 }
